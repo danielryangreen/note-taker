594e60af242e9c06d31f5b80e81c837aebac5e56
Dan Green <dan.ryan.green@gmail.com>
2021-02-28 06:38:12 -0700

    Create API route to post notes to db json

--- server.js
+++ server.js
@@ -28,6 +28,13 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  console.log(newNote);
+  noteData.push(newNote);
+  res.json(noteData);
+});
+
 // listener
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
