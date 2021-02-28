236a177e445abd550204d7883b43773b710a29f0
Dan Green <dan.ryan.green@gmail.com>
2021-02-27 17:57:05 -0700

    Create API route to get notes from db json

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const path = require('path');
 
+// data
+const noteData = require('./db/db');
+
 // configuration
 const app = express();
 const PORT = 8080;
@@ -10,12 +13,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // router
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(noteData);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 // listener
